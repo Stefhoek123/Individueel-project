3bca68cb811db5cb948f4be490afbb4425e0b031
Stefhoek123 <stefaniehoeksma@gmail.com>
2024-11-04 15:44:40 +0100

    New project

--- Backend/Interface/obj/Debug/net8.0/Interface.AssemblyInfo.cs
+++ Backend/Interface/obj/Debug/net8.0/Interface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f204bc596f0c70387448f73134ab585b91fc8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3501c1b3949e73f6a8db71240eb061ace60b6e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
