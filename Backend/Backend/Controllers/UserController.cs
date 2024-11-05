08c2fb03a03e3aa82eaf72f4389000988ae6b77a
Stefhoek123 <stefaniehoeksma@gmail.com>
2024-11-05 14:15:25 +0100

    changed url

--- Backend/Backend/Controllers/UserController.cs
+++ Backend/Backend/Controllers/UserController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
         // Dependency Injection of user container
