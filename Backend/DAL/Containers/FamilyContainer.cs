43d94702081ead4b348835538c5d74e1f3fb1a7f
Stefhoek123 <stefaniehoeksma@gmail.com>
2024-11-22 15:15:25 +0100

    added family

--- Backend/DAL/Containers/FamilyContainer.cs
+++ Backend/DAL/Containers/FamilyContainer.cs
@@ -6,33 +6,33 @@ namespace DAL.Containers;
 
 public class FamilyContainer : IFamilyContainer
 {
-    // Dependency injection of family repository
-    private readonly IFamilyContainer _familyRepository;
-    public FamilyContainer(IFamilyContainer familyRepository)
+    // Dependency injection of user repository
+    private readonly IFamilyRepository _familyRepository;
+    public FamilyContainer(IFamilyRepository familyRepository)
     {
         _familyRepository = familyRepository;
     }
 
-    // GET for getting all families
+    // GET for getting all users
     public IEnumerable<FamilyDto> GetAllFamilies()
     {
         return _familyRepository.GetAllFamilies()
             .Select(Mappers.FamilyMapper.ToDto);
     }
 
-    // GET for getting a family based on a family ID
+    // GET for getting a user based on a User ID
     public FamilyDto GetFamilyById(Guid id)
     {
         return Mappers.FamilyMapper.ToDto(_familyRepository.GetFamilyById(id));
     }
 
-    // GET for finding a family based on Name
+    // GET for finding a User based on his Email or Name
     public IEnumerable<FamilyDto> SearchFamilyByName(string search)
     {
         return string.IsNullOrEmpty(search) ? GetAllFamilies() : _familyRepository.SearchFamilyByName(search).Select(Mappers.FamilyMapper.ToDto);
     }
 
-    // POST Creation of family
+    // POST Creation of user
     public void CreateFamily(FamilyDto familydto)
     {
         var family = Mappers.FamilyMapper.ToModel(familydto);
@@ -40,13 +40,13 @@ public class FamilyContainer : IFamilyContainer
         _familyRepository.CreateFamily(family);
     }
 
-    // PUT for updating a family
+    // PUT for updating a user
     public void UpdateFamily(FamilyDto family)
     {
         _familyRepository.UpdateFamily(Mappers.FamilyMapper.ToModel(family));
     }
 
-    // DELETE of a family op basis van user ID 
+    // DELETE of a user op basis van user ID 
     public void DeleteFamilyById(Guid id)
     {
         _familyRepository.DeleteFamilyById(id);
