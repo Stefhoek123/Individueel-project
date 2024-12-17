912d9b88592a706d074f2a6885181ceffdc53885
Stefhoek123 <stefaniehoeksma@gmail.com>
2024-12-17 13:53:08 +0100

    Added framework cypress

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23360430282f9b69920d02e404900b0cb89d476a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834803042ca51a359f0deb08c11ec8c3a9d7f931")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
